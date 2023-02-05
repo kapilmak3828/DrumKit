f32f25b0e686df76965a2f09d903be063b9ab6e1
kapil makhija <kapilm3828@gmail.com>
2023-02-06 02:32:28 +0530

    first commit sucessfully

---
+++ index.js
@@ -0,0 +1,19 @@
+function PlayAudio(e){
+const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+if(!audio) return;
+audio.currentTime = 0;
+audio.play();
+key.classList.add("playing");
+}    
+
+function removetransition(e){
+  if(e.propertyName !== 'transform') return; //select only transform property
+
+ this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener("transitionend", removetransition));
+window.addEventListener("keydown" , PlayAudio);
